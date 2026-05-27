2ffcfaefead105859850facd9c275631dcfcd29a
gabysdev <gabymais4@gmail.com>
2026-05-27 19:59:57 -0300

    Add.04

---
+++ exe04.py
@@ -0,0 +1,11 @@
+def maior_numero(a,b):
+    if a> b:
+        print(f"o numero {a} é maior")
+    else:
+        print(f"o numero {b} é maior")
+
+#Exemplo:
+
+
+maior_numero(a=3, b=9)
+maior_numero(b=9, a=3)
